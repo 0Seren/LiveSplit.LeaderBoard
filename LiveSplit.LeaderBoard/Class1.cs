89d81d9ff16612bbccb4098ea105b6ddc2a2a15c
0Seren <allenboss95@gmail.com>
2016-10-29 13:38:23 -0400

    Add project files.

---
+++ LiveSplit.LeaderBoard/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveSplit.LeaderBoard
+{
+    public class Class1
+    {
+    }
+}
